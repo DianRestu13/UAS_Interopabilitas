65180f2193359135916c7b074ee883ee284fa798
DianRestu <restukhoirunnisa13@gmail.com>
2025-12-16 13:16:11 +0700

    vendor A

--- server.js
+++ server.js
@@ -5,11 +5,36 @@ const app = express();
 app.use(cors());
 
 const vendorAData = [
-  { kd_produk: "A001", nm_brg: "Kopi Bubuk 100g", hrg: "15000", ket_stok: "ada" },
-  { kd_produk: "A002", nm_brg: "Kerupuk Rambak 250g", hrg: "40000", ket_stok: "habis" },
-  { kd_produk: "A003", nm_brg: "Susu Kental Manis", hrg: "10000", ket_stok: "ada" },
-  { kd_produk: "A004", nm_brg: "Gula Pasir 100g", hrg: "10000", ket_stok: "ada" },
-  { kd_produk: "A005", nm_brg: "Telur Ayam Kampung 1kg", hrg: "25000", ket_stok: "habis" }
+  { 
+    kd_produk: "A001",
+    nm_brg: "Kopi Bubuk 100g",
+    hrg: "15000",
+    ket_stok: "ada"
+  },
+  { 
+    kd_produk: "A002",
+    nm_brg: "Kerupuk Rambak 250g",
+    hrg: "40000",
+    ket_stok: "habis"
+  },
+  { 
+    kd_produk: "A003",
+    nm_brg: "Susu Kental Manis",
+    hrg: "10000",
+    ket_stok: "ada"
+  },
+  { 
+    kd_produk: "A004",
+    nm_brg: "Gula Pasir 100g",
+    hrg: "10000",
+    ket_stok: "ada"
+  },
+  { 
+    kd_produk: "A005",
+    nm_brg: "Telur Ayam Kampung 1kg",
+    hrg: "25000",
+    ket_stok: "habis"
+  }
 ];
 
 app.get('/', (req, res) => {
